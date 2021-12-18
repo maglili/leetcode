8427183c0e97e730194b9c634ca3e7a20e34c6af
maglili <gibson870811@gmail.com>
2021-12-18 18:45:32 +0800

    rewrite 938

--- algo/938. Range Sum of BST.c
+++ algo/938. Range Sum of BST.c
@@ -7,30 +7,23 @@
  * };
  */
 
-int DFS(struct TreeNode* root, int low, int high)
-{
-    int val1 = 0, val2 = 0;
+int rangeSumBST(struct TreeNode* root, int low, int high){
+    
+    // base case
     if (root == NULL)
         return 0;
-        
-    // printf("%i\n", root->val);
-        
+    
+    // Recursive case
+    int val1 = 0, val2 = 0;
+    
     if (root->left != NULL)
-        val1 = DFS(root->left, low, high);
+        val1 = rangeSumBST(root->left, low, high);
         
     if (root->right != NULL)
-        val2 = DFS(root->right, low, high);
+        val2 = rangeSumBST(root->right, low, high);
     
     if (root->val >= low && root->val <= high)
         return root->val + val1 + val2;
     else
         return val1 + val2;
 }
-
-
-int rangeSumBST(struct TreeNode* root, int low, int high){
-    int res;
-    res = DFS(root, low, high);
-    return res;
-}
-
