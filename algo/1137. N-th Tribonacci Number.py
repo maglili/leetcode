de569a6736b830fc87528f8970b93e18adf60730
maglili <gibson870811@gmail.com>
2023-10-20 01:04:17 +0800

    add 1137

---
+++ algo/1137. N-th Tribonacci Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b, c = 1, 0, 0
+        for _ in range(n):
+            a, b, c = b, c, a+b+c
+        return c
